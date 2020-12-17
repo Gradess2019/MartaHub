368ac9cfdfe418d3453d6d5c4fd100285c4605cf
Stepan <trofimovjava@gmail.com>
2020-12-18 02:55:34 +0300

    Add RPCs to UPickupableComponent

--- Source/MartaHub/Private/Components/PickupableComponent.cpp
+++ Source/MartaHub/Private/Components/PickupableComponent.cpp
@@ -2,34 +2,47 @@
 
 #include "Components/PickupableComponent.h"
 
+
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Libraries/PickupHelper.h"
+
 DEFINE_LOG_CATEGORY(LogPickupableComponent);
 
 UPickupableComponent::UPickupableComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
+
+	PickupProfileName = FName("PickupedActor");
+	ClearProfileName = FName("PhysicsActor");
+
+	ClassFilter = AActor::StaticClass();
+	
+	CheckOverlapsRate = 0.1f;
+	InterpolationSpeed = 10.f;
 }
 
 void UPickupableComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	const auto bSuccess = SetupParent();
+	const auto bSuccess = SetupRoot();
 	if (!bSuccess) { return; }
-	
+
 	SetupOffset();
 	CacheBound();
 }
 
-bool UPickupableComponent::SetupParent_Implementation()
+bool UPickupableComponent::SetupRoot_Implementation()
 {
-	Parent = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
-	if (!IsValid(Parent))
+	Root = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	if (!IsValid(Root))
 	{
 		UE_LOG(LogPickupableComponent, Error, TEXT("RootComponent isn't UStaticMeshComponent!"));
 		DestroyComponent();
 		return false;
 	}
-	
+
 	return true;
 }
 
@@ -48,7 +61,145 @@ void UPickupableComponent::CacheBound_Implementation()
 {
 	auto Min = FVector();
 	auto Max = FVector();
-	Parent->GetLocalBounds(Min, Max);
+	Root->GetLocalBounds(Min, Max);
 
 	CachedBound = FMath::Abs(Min.X) + FMath::Abs(Max.X);
+}
+
+void UPickupableComponent::CheckOverlaps_Implementation()
+{
+	TArray<AActor*> OverlappingActors;
+	GetOwner()->GetOverlappingActors(OverlappingActors, ClassFilter);
+
+	const auto Length = OverlappingActors.Num();
+	if (Length > 0) { return; }
+
+	ClearCheckOverlapsTimer();
+	Root->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	Root->SetCollisionResponseToChannel(ECC_PhysicsBody, ECR_Block);
+}
+
+void UPickupableComponent::ClearCheckOverlapsTimer()
+{
+	GetWorld()->GetTimerManager().ClearTimer(CheckOverlapsTimer);
+}
+
+// IPickupable begin
+void UPickupableComponent::SetPickupLocation_Implementation(const FVector& OwnerLocation, const FVector& OwnerDirection)
+{
+	auto Target = GetOwner();
+	const auto Alpha = UGameplayStatics::GetWorldDeltaSeconds(this) * InterpolationSpeed;
+
+	const auto Location = UPickupHelper::CalculatePickupLocation(
+		Target,
+		OwnerLocation,
+		OwnerDirection,
+		Offset,
+		CachedBound,
+		Alpha
+	);
+
+	const auto Rotation = UPickupHelper::CalculatePickupRotation(
+		Target,
+		OwnerDirection,
+		Alpha
+	);
+
+	Target->SetActorLocation(Location);
+	Target->SetActorRotation(Rotation);
+}
+
+void UPickupableComponent::PreparePickup_Implementation()
+{
+	const auto bStandalone = UKismetSystemLibrary::IsStandalone(this);
+	if (bStandalone)
+	{
+		ClientPreparePickup();
+	}
+	else
+	{
+		ServerPreparePickup();
+	}
+}
+
+void UPickupableComponent::ClearPickup_Implementation()
+{
+	const auto bStandalone = UKismetSystemLibrary::IsStandalone(this);
+	if (bStandalone)
+	{
+		ClientClearPickup();
+	}
+	else
+	{
+		ServerClearPickup();
+	}
+}
+
+void UPickupableComponent::Throw_Implementation(float Force)
+{
+	const auto Direction = GetOwner()->GetActorForwardVector();
+	const auto TotalForce = Root->GetMass() * Force;
+	const auto Impulse = TotalForce * Direction;
+	Root->AddImpulse(Impulse);
+}
+// IPickupable end
+
+// PrepareForPickup() RPCs
+void UPickupableComponent::ClientPreparePickup_Implementation()
+{
+	Root->SetSimulatePhysics(false);
+	Root->SetCollisionProfileName(PickupProfileName);
+
+	Root->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	Root->SetCollisionResponseToChannel(ECC_PhysicsBody, ECR_Overlap);
+
+	const auto bLoop = true;
+	GetWorld()->GetTimerManager().SetTimer(
+		CheckOverlapsTimer,
+		this,
+		&UPickupableComponent::CheckOverlaps,
+		CheckOverlapsRate,
+		bLoop
+	);
+}
+
+void UPickupableComponent::ServerPreparePickup_Implementation()
+{
+	MulticastPreparePickup();
+	GetOwner()->SetReplicates(false);
+}
+
+bool UPickupableComponent::ServerPreparePickup_Validate()
+{
+	return true;
+}
+
+void UPickupableComponent::MulticastPreparePickup_Implementation()
+{
+	ClientPreparePickup();
+}
+
+// ClearPickup() RPCs
+void UPickupableComponent::ClientClearPickup_Implementation()
+{
+	Root->SetSimulatePhysics(true);
+	Root->SetCollisionProfileName(ClearProfileName);
+	Root->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	ClearCheckOverlapsTimer();
+}
+
+void UPickupableComponent::ServerClearPickup_Implementation()
+{
+	GetOwner()->SetReplicates(true);
+	MulticastClearPickup();
+}
+
+bool UPickupableComponent::ServerClearPickup_Validate()
+{
+	return true;
+}
+
+void UPickupableComponent::MulticastClearPickup_Implementation()
+{
+	ClientClearPickup();
 }
--- Source/MartaHub/Public/Components/PickupableComponent.h
+++ Source/MartaHub/Public/Components/PickupableComponent.h
@@ -4,49 +4,163 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Interfaces/Pickupable.h"
 #include "PickupableComponent.generated.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(LogPickupableComponent, Log, All);
 
+class UStaticMeshComponent;
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class MARTAHUB_API UPickupableComponent : public UActorComponent
+class MARTAHUB_API UPickupableComponent : public UActorComponent, public IPickupable
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	UPickupableComponent();
 
 protected:
 
-	UPROPERTY(BlueprintReadWrite)
+	UPROPERTY(BlueprintReadWrite, Category = "Pickupable Component")
 	FVector Offset;
 
-	UPROPERTY(BlueprintReadWrite)
+	UPROPERTY(BlueprintReadWrite, Category = "Pickupable Component")
 	float CachedBound;
 
-	UPROPERTY()
-	class UStaticMeshComponent* Parent;
+	UPROPERTY(BlueprintReadWrite, Category = "Pickupable Component")
+	UStaticMeshComponent* Root;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Pickupable Component")
+	FTimerHandle CheckOverlapsTimer;
+
+	UPROPERTY(
+		BlueprintReadWrite,
+		EditAnywhere,
+		Category = "Pickupable Component | Collision"
+	)
+	FName PickupProfileName;
+
+	UPROPERTY(
+		BlueprintReadWrite,
+		EditAnywhere,
+		Category = "Pickupable Component | Collision"
+	)
+	FName ClearProfileName;
+
+	/** Class Filter for checking overlaps */
+	UPROPERTY(
+		BlueprintReadWrite,
+		EditAnywhere,
+		Category = "Pickupable Component | Overlap"
+	)
+	TSubclassOf<AActor> ClassFilter;
+
+	UPROPERTY(
+		BlueprintReadWrite,
+		EditAnywhere,
+		Category = "Pickupable Component | Overlap"
+	)
+	float CheckOverlapsRate;
+
+	/** SetPickupLocation interpolation speed. The higher, the faster */
+	UPROPERTY(
+		BlueprintReadWrite,
+		EditAnywhere,
+		Category = "Pickupable Component | Interpolation"
+	)
+	float InterpolationSpeed;
 
 	virtual void BeginPlay() override;
 
 	UFUNCTION(
-		BlueprintCallable, 
+		BlueprintCallable,
 		BlueprintNativeEvent,
 		Category = "Pickupable Component"
 	)
-	bool SetupParent();
+	bool SetupRoot();
 
 	UFUNCTION(
-		BlueprintCallable, 
+		BlueprintCallable,
 		BlueprintNativeEvent,
 		Category = "Pickupable Component"
 	)
 	void SetupOffset();
 
 	UFUNCTION(
-		BlueprintCallable, 
+		BlueprintCallable,
 		BlueprintNativeEvent,
 		Category = "Pickupable Component"
 	)
 	void CacheBound();
+
+	UFUNCTION(
+		BlueprintCallable,
+		BlueprintNativeEvent,
+		Category = "Pickupable Component"
+	)
+	void CheckOverlaps();
+
+	UFUNCTION(
+        BlueprintCallable,
+        Category = "Pickupable Component"
+    )
+    void ClearCheckOverlapsTimer();
+
+public:
+	// IPickupable begin
+	virtual void SetPickupLocation_Implementation(const FVector& OwnerLocation, const FVector& OwnerDirection) override;
+	virtual void PreparePickup_Implementation() override;
+	virtual void ClearPickup_Implementation() override;
+	virtual void Throw_Implementation(float Force) override;
+	// IPickupable end
+
+protected:
+	// IPickupable replication begin
+
+	// PrepareForPickup() RPCs
+	UFUNCTION(
+		BlueprintCallable,
+		BlueprintNativeEvent,
+		Category = "Pickupable Component"
+	)
+	void ClientPreparePickup();
+
+	UFUNCTION(
+		BlueprintCallable,
+		Server,
+		WithValidation,
+		Reliable
+	)
+	void ServerPreparePickup();
+
+	UFUNCTION(
+		BlueprintCallable,
+		NetMulticast,
+		Reliable
+	)
+	void MulticastPreparePickup();
+
+	// ClearPickup() RPCs
+	UFUNCTION(
+		BlueprintCallable,
+		BlueprintNativeEvent,
+		Category = "Pickupable Component"
+	)
+	void ClientClearPickup();
+
+	UFUNCTION(
+		BlueprintCallable,
+		Server,
+		WithValidation,
+		Reliable
+	)
+	void ServerClearPickup();
+
+	UFUNCTION(
+		BlueprintCallable,
+		NetMulticast,
+		Reliable
+	)
+	void MulticastClearPickup();
+	// IPickupable replication end
 };
