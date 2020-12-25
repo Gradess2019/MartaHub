e94dc8b83dca744186530ac8ce19dd9705bee9c1
Stepan <trofimovjava@gmail.com>
2020-12-25 23:56:49 +0300

    Add ReferenceCount to USnapshotBase

--- Source/MartaHub/Public/Snapshots/SnapshotBase.h
+++ Source/MartaHub/Public/Snapshots/SnapshotBase.h
@@ -14,5 +14,25 @@ UCLASS()
 class MARTAHUB_API USnapshotBase : public UObject, public IRestorer
 {
 	GENERATED_BODY()
-	
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "Snapshot Base")
+	int ReferenceCount;
+
+public:
+	/** Increase ReferenceCount */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Snapshot Base")
+	void Take();
+
+	/** Decrease ReferenceCount */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Snapshot Base")
+    void Release();
+
+	/** Indicates if restore is available */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Snapshot Base")
+	bool CanRestore();
+
+	/** Indicates if restore is available */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Snapshot Base")
+	int GetReferenceCount();
 };
