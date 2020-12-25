49772ecbe321a341836453b5487443785501dda8
Stepan <trofimovjava@gmail.com>
2020-12-25 23:55:13 +0300

    Add UDefaultSnapshotFactory

---
+++ Source/MartaHub/Private/Factories/DefaultSnapshotFactory.cpp
@@ -0,0 +1,32 @@
+﻿// Gradess Games. All rights reserved.
+
+#include "Factories/DefaultSnapshotFactory.h"
+
+#include "Snapshots/ActorPhysicsSnapshot.h"
+
+
+USnapshotBase* UDefaultSnapshotFactory::CreateSnapshot_Implementation(TSubclassOf<USnapshotBase> Class)
+{
+	if (Class->IsChildOf(UComponentPhysicsSnapshot::StaticClass()))
+	{
+		return Execute_CreateComponentPhysicsSnapshot(this);
+	}
+
+	if (Class->IsChildOf(UActorPhysicsSnapshot::StaticClass()))
+	{
+		return Execute_CreateActorPhysicsSnapshot(this);
+	}
+
+	return nullptr;
+}
+
+USnapshotBase* UDefaultSnapshotFactory::CreateComponentPhysicsSnapshot_Implementation()
+{
+	return NewObject<UComponentPhysicsSnapshot>();
+}
+
+USnapshotBase* UDefaultSnapshotFactory::CreateActorPhysicsSnapshot_Implementation()
+{
+	return NewObject<UActorPhysicsSnapshot>();
+
+}
