ed9d63839786a641f0bf72b3f37a2681a0da2ec9
Stepan <trofimovjava@gmail.com>
2020-12-30 23:43:55 +0300

    Update SimpleRadio plugin

---
+++ Plugins/SimpleRadio/Source/SimpleRadio/Private/Actors/Radio.cpp
@@ -0,0 +1,220 @@
+// Gradess Games. All rights reserved.
+
+#include "Actors/Radio.h"
+#include "MediaSoundComponent.h"
+#include "RadioStationPlaylist.h"
+#include "MediaPlayer.h"
+#include "Interfaces/RadioReplicator.h"
+#include "Kismet/GameplayStatics.h"
+#include "Libraries/RadioHelper.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Net/UnrealNetwork.h"
+
+ARadio::ARadio()
+{
+	MediaSoundComponent = CreateDefaultSubobject<UMediaSoundComponent>(FName("MediaSound"));
+	MediaSoundComponent->SetupAttachment(RootComponent);
+
+	const auto MediaPlayerPath = TEXT("MediaPlayer'/SimpleRadio/Media/MP_Radio.MP_Radio'");
+	static ConstructorHelpers::FObjectFinder<UMediaPlayer> MediaPlayerAsset(MediaPlayerPath);
+	const auto MediaPlayer = MediaPlayerAsset.Object;
+#if WITH_EDITOR
+	MediaSoundComponent->SetDefaultMediaPlayer(MediaPlayer);
+#else
+	MediaSoundComponent->SetMediaPlayer(MediaPlayer);
+#endif
+
+	MediaSoundComponent->bAllowSpatialization = true;
+
+	const auto AttenuationPath = TEXT("SoundAttenuation'/SimpleRadio/Sound/ATT_Radio.ATT_Radio'");
+	static ConstructorHelpers::FObjectFinder<USoundAttenuation> AttenuationAsset(AttenuationPath);
+	const auto Attenuation = AttenuationAsset.Object;
+	MediaSoundComponent->AttenuationSettings = Attenuation;
+
+	MinVolume = 0.f;
+	MaxVolume = 1.f;
+
+	bAutoPlay = true;
+
+	bStaticMeshReplicateMovement = true;
+	bReplicates = true;
+}
+
+void ARadio::BeginPlay()
+{
+	Super::BeginPlay();
+
+	const auto bServer = HasAuthority();
+	if (!bServer) { return; }
+
+	BindUnlockFunction();
+	SetupStreams();
+	SetupVolume();
+}
+
+void ARadio::BindUnlockFunction_Implementation()
+{
+	FScriptDelegate UnlockDelegate;
+	UnlockDelegate.BindUFunction(this, "Unlock");
+
+	auto MediaPlayer = GetMedia();
+	MediaPlayer->OnMediaOpened.Add(UnlockDelegate);
+	MediaPlayer->OnMediaOpenFailed.Add(UnlockDelegate);
+}
+
+void ARadio::SetupStreams_Implementation()
+{
+	Streams = URadioStationPlaylist::GetStreams();
+
+	if (!bAutoPlay) { return; }
+
+	PlayById(0);
+}
+
+void ARadio::SetupVolume_Implementation()
+{
+	const auto InitialVolume = URadioHelper::GetMediaVolume(MediaSoundComponent);
+	SetVolume(InitialVolume);
+}
+
+void ARadio::SetVolume_Implementation(const float NewVolume)
+{
+	Volume = NewVolume;
+}
+
+void ARadio::Open_Implementation(const FString& URL)
+{
+	auto MediaPlayer = GetMedia();
+	MediaPlayer->Close();
+	MediaPlayer->OpenUrl(URL);
+}
+
+UMediaPlayer* ARadio::GetMedia_Implementation()
+{
+	return MediaSoundComponent->GetMediaPlayer();
+}
+
+void ARadio::Play_Implementation(const FString& URL)
+{
+	const auto bAuthority = HasAuthority();
+	if (bAuthority)
+	{
+		if (bLock || CurrentStream.Equals(URL, ESearchCase::IgnoreCase)) { return; }
+
+		CurrentStream = URL;
+
+#if !UE_SERVER
+		OnRep_CurrentStream();
+#endif
+		Lock();
+
+#if UE_SERVER
+		UE_LOG(LogTemp, Warning, TEXT("Dedicated server"));
+		FTimerHandle UnlockTimer;
+		GetWorldTimerManager().SetTimer(
+			UnlockTimer,
+			this,
+			&ARadio::Unlock,
+			2.f
+		);
+#endif
+	}
+	else
+	{
+		const auto RadioReplicator = GetRadioReplicator();
+		if (!IsValid(RadioReplicator)) { return; }
+
+		IRadioReplicator::Execute_Play(RadioReplicator, this, URL);
+	}
+}
+
+void ARadio::Lock_Implementation()
+{
+	bLock = true;
+}
+
+void ARadio::OnRep_CurrentStream_Implementation()
+{
+	Open(CurrentStream);
+}
+
+void ARadio::Unlock_Implementation()
+{
+	bLock = false;
+}
+
+UObject* ARadio::GetRadioReplicator_Implementation()
+{
+	const auto PlayerController = UGameplayStatics::GetPlayerController(this, 0);
+	const auto bImplements = PlayerController->Implements<URadioReplicator>();
+	return bImplements ? PlayerController : nullptr;
+}
+
+void ARadio::PlayNext_Implementation()
+{
+	const auto CurrentStreamId = Streams.Find(CurrentStream);
+	const auto NextStreamId = (CurrentStreamId + 1) % Streams.Num();
+
+	PlayById(NextStreamId);
+}
+
+void ARadio::PlayById_Implementation(const int Id)
+{
+	const auto bValidIndex = Streams.IsValidIndex(Id);
+	if (!bValidIndex) { return; }
+
+	const auto NewStream = Streams[Id];
+	Play(NewStream);
+}
+
+void ARadio::PlayPrevious_Implementation()
+{
+	const auto CurrentStreamId = Streams.Find(CurrentStream);
+	const auto PreviousStreamId = CurrentStreamId - 1 < 0 ? Streams.Num() - 1 : CurrentStreamId - 1;
+
+	PlayById(PreviousStreamId);
+}
+
+void ARadio::AdjustVolume_Implementation(const float Delta)
+{
+	const auto bServer = HasAuthority();
+	if (bServer)
+	{
+		const float NewVolume = CalculateNewVolume(Delta);
+		SetVolume(NewVolume);
+		OnRep_Volume();
+	}
+	else
+	{
+		const auto RadioReplicator = GetRadioReplicator();
+		if (!IsValid(RadioReplicator)) { return; }
+
+		IRadioReplicator::Execute_AdjustVolume(RadioReplicator, this, Delta);
+	}
+}
+
+float ARadio::CalculateNewVolume_Implementation(const float Delta)
+{
+	const auto CurrentVolume = URadioHelper::GetMediaVolume(MediaSoundComponent);
+	const auto NewVolume = FMath::Clamp(CurrentVolume + Delta, MinVolume, MaxVolume);
+	return NewVolume;
+}
+
+void ARadio::OnRep_Volume_Implementation()
+{
+	SetVolumeInternal(Volume);
+}
+
+void ARadio::SetVolumeInternal_Implementation(const float NewVolume)
+{
+	URadioHelper::SetMediaVolume(MediaSoundComponent, NewVolume);
+}
+
+void ARadio::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ARadio, CurrentStream);
+	DOREPLIFETIME(ARadio, Volume);
+	DOREPLIFETIME_CONDITION(ARadio, Streams, COND_InitialOnly);
+}
---
+++ Plugins/SimpleRadio/Source/SimpleRadio/Public/Actors/Radio.h
@@ -0,0 +1,132 @@
+// Gradess Games. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "Radio.generated.h"
+
+class UMediaSoundComponent;
+class UMediaPlayer;
+
+UCLASS(BlueprintType, Blueprintable)
+class SIMPLERADIO_API ARadio : public AStaticMeshActor
+{
+	GENERATED_BODY()
+
+public:
+	ARadio();
+
+protected:
+	/** Media sound component*/
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Radio")
+	UMediaSoundComponent* MediaSoundComponent;
+
+	/** Should play media on begin play */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Radio")
+	bool bAutoPlay;
+
+	/** Indicates whether radio is connection to stream. Prevents crash */
+	UPROPERTY(BlueprintReadWrite, Category = "Radio | Stream")
+	bool bLock;
+
+	/** Current connected stream */
+	UPROPERTY(BlueprintReadWrite, ReplicatedUsing=OnRep_CurrentStream, Category = "Radio | Stream")
+	FString CurrentStream;
+
+	/** Available streams to connect */
+	UPROPERTY(BlueprintReadWrite, Replicated, Category = "Radio | Stream")
+	TArray<FString> Streams;
+
+	/** Current volume of radio */
+	UPROPERTY(BlueprintReadWrite, ReplicatedUsing=OnRep_Volume, Category = "Radio | Volume")
+	float Volume;
+
+	/** Min volume of radio */
+	UPROPERTY(BlueprintReadWrite, Category = "Radio | Volume")
+	float MinVolume;
+
+	/** Max volume of radio */
+	UPROPERTY(BlueprintReadWrite, Category = "Radio | Volume")
+	float MaxVolume;
+
+	virtual void BeginPlay() override;
+
+	/** Binds Unlock() to OnMediaOpened and OnMediaOpenFailed delegates */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, BlueprintAuthorityOnly, Category = "Radio | Stream")
+	void BindUnlockFunction();
+
+	/** Loads streams from configuration file */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, BlueprintAuthorityOnly, Category = "Radio | Stream")
+	void SetupStreams();
+
+	/** Sets initial volume */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, BlueprintAuthorityOnly, Category = "Radio | Volume")
+	void SetupVolume();
+
+public:
+	/** Sets volume */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, BlueprintAuthorityOnly, Category = "Radio | Volume")
+	void SetVolume(const float NewVolume);
+
+protected:
+	/** Opens stream URL */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Radio | Stream")
+	void Open(const FString& URL);
+
+public:
+	/** Returns current Media Player */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Radio")
+	UMediaPlayer* GetMedia();
+
+	/** Play new stream */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Radio | Stream")
+	void Play(const FString& URL);
+
+protected:
+	/** Locks Play() while radio is connecting to new stream */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, BlueprintAuthorityOnly, Category = "Radio | Stream")
+	void Lock();
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Radio | Stream")
+	void OnRep_CurrentStream();
+
+	/** Unlocks Play() method */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, BlueprintAuthorityOnly, Category = "Radio | Stream")
+	void Unlock();
+
+public:
+	/** Returns object that is in charge of radio replication (used for calling RPCs) */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Radio")
+	UObject* GetRadioReplicator();
+
+	/** Plays next stream */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Radio | Stream")
+	void PlayNext();
+
+	/** Plays stream by index (in Streams array) */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Radio | Stream")
+	void PlayById(const int Id);
+
+	/** Plays previous stream */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Radio | Stream")
+	void PlayPrevious();
+
+	/** Decreases or increases volume of radio */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Radio | Volume")
+	void AdjustVolume(const float Delta = 0.05f);
+
+protected:
+	/** Calculates new volume */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Radio | Volume")
+	float CalculateNewVolume(const float Delta);
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Radio | Volume")
+	void OnRep_Volume();
+
+	/** Set volume of media sound component. Doesn't replicate Volume variable, use SetVolume() instead.  */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Radio | Volume")
+	void SetVolumeInternal(const float NewVolume);
+
+	virtual void GetLifetimeReplicatedProps(::TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+};
